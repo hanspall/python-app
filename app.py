6722b2dafa24996999d6f8f46f1e410b96e82774
Gurpreet Singh <erhanspal@outlook.com>
2020-04-19 12:21:51 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+# name = input('what is your name? ')
+# color = input('what is your favourite color? ')
+# print(name + ' likes '+ color)
+
+# birth_year = input('birth year:')
+# print(type(birth_year))
+# age = input(2019 - int(birth_year))
+# print(age)
+
+
+# user_wieght_pound = input('what is your weight in pounds? ')
+# convert = 0.45
+# Weight_kilogram = input(convert * float(user_wieght_pound))
+# print(Weight_kilogram)
+
+# email = '''
+# Hi Sam,
+
+# We received your request. I will update you once it resolved.
+
+# Thanks
+# The Support Team
+# '''
+# print(email)
+
+# course = 'Python for Begginers'
+# another = course[:]
+# print(course[1:-1])
+
+
+# first = 'Gurpreet'
+# last = 'Singh'
+# msg = f'{first} [{last}] is a coder'
+# print(msg)
+
+# course = 'Python for Begginers'
+# print(course.replace('g','t'))
+# print(course.find('t'))
+
+course = 'python for beginners'
+print(course.title())
+
+
+
+
+
